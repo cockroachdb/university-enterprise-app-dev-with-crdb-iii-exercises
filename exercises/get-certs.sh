82ce7e0100a3315144329f013e974caefcd945d2
kikiya <kikia@cockroachlabs.com>
2023-12-11 02:50:41 -0500

    added cert script (#6)

---
+++ exercises/get-certs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Downloads the required certificate for the provided cluster, with a few
+# assumptions:
+# 1. bash, ccloud, and jq[1] are installed and on the user's $PATH
+# 2. Users are already logged in via `ccloud auth login`
+# 3. The provided cluster exists
+#
+# [1] https://jqlang.github.io/jq/
+
+set -euo pipefail
+
+if [[ "$UID" == "0" ]]; then
+  echo "Don't run this command as root." >&1
+  exit 2
+fi
+
+name=${1:-""}
+if [[ -z "$name" || "$name" == "help" || "$name" == "--help" || "$name" == "-h" ]]; then
+  cat >&1 <<EOF
+Usage: $0 cluster_name
+
+Example: $0 dijon-clustered
+EOF
+  exit 1
+fi
+
+cluster_id=$(ccloud cluster info --quiet --output json "$name" | jq --raw-output '.id')
+curl \
+  --silent \
+  --create-dirs \
+  --output "$HOME/.postgresql/root.crt" \
+  "https://cockroachlabs.cloud/clusters/$cluster_id/cert"
